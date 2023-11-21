e1160279b24cc3ca831e671fbcac533fa75b6646
Pratik Mhatre <107295431+pratikkmhatre@users.noreply.github.com>
2023-11-21 11:17:53 +0530

    databse error

--- util/database.js
+++ util/database.js
@@ -1,12 +1,13 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("prescription", "root", "sharp", {
+const sequelize = new Sequelize("sql8662649", "sql8662649", "7pZZVg1PQ4", {
   dialect: "mysql",
-  host: "localhost",
+  host: "sql8.freemysqlhosting.net",
 });
-// const sequelize = new Sequelize("sql8662649", "sql8662649", "7pZZVg1PQ4", {
+
+// const sequelize = new Sequelize("prescription", "root", "sharp", {
 //   dialect: "mysql",
-//   host: "sql8.freemysqlhosting.net",
+//   host: "localhost",
 // });
 
 module.exports = sequelize;
